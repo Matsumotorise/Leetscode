35d8686568cfc7002ba4e5d41877e78c49cd6efb
Andrew Chan <andrewclchan211@vt.edu>
2022-07-03 15:12:38 -0400

    Time: 474 ms (36.53%), Space: 20.8 MB (77.94%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        # bfs this path
+        from collections import deque
+        queue = deque([start])
+        visited = set()
+        
+        while queue:
+            idx = queue.popleft()
+            # exit conds
+            if idx in visited:
+                continue
+            if arr[idx] == 0:
+                return True
+            
+            visited.add(idx)
+            
+            # add children
+            for ci in (idx+arr[idx], idx - arr[idx]):
+                if 0 <= ci < len(arr): # if in bounds
+                    queue.append(ci)
+                    
+            
+            
+        return False
+                
+            
+            
+        
