ae96390a848576f47666858bf54fd9ef221dbb4b
Andrew Chan <andrewclchan211@vt.edu>
2022-07-19 12:09:01 -0400

    Time: 52 ms (57.37%), Space: 14.2 MB (76.81%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        lst = []
+        curStr = []
+        
+        
+        def dfs(numOpen=0, numClose=0):
+            # Based case
+            if numClose == n:
+                lst.append("".join(curStr))
+                
+            # add children
+            if numOpen < n:
+                curStr.append("(")
+                dfs(numOpen+1, numClose)
+                curStr.pop()
+            if numOpen > numClose:
+                curStr.append(")")
+                dfs(numOpen, numClose+1)
+                curStr.pop()
+
+        dfs()
+        
+        return lst
+                
+            
