16182f26527d0667cba27e229c09b6e2e644bb4a
Andrew Chan <andrewclchan211@vt.edu>
2022-08-05 15:12:16 -0400

    Time: 691 ms (48.91%), Space: 25.7 MB (98.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,23 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        from collections import Counter
-        return any(map(lambda x: x>=2, Counter(nums).values()))
+        # O(n) space, O(n) time
+        #from collections import Counter
+        #return any(map(lambda x: x>=2, Counter(nums).values()))
+    
+        ''' #TLE O(n^2) time, O(1) space
+        for i, n1 in enumerate(nums):
+            for j, n2 in enumerate(nums):
+                if i != j and n1 == n2:
+                        return True
+        return False
+        '''
         
+        nums.sort()
+        for n1, n2 in zip(nums[1:], nums):
+            if n1 == n2:
+                return True
+        
+    
         #TLE
         #from itertools import combinations
         #return any(x[0] == x[1] for x in combinations(nums, 2))
