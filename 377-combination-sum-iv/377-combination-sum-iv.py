c1387ae86e6b84ce9d50e3167d472884fe1b8f38
Andrew Chan <andrewclchan211@vt.edu>
2022-08-05 16:47:58 -0400

    Time: 58 ms (62.16%), Space: 14.3 MB (7.44%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -2,18 +2,13 @@ class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
         
-        dp = {}
         
-        
-        
-        
+        @lru_cache(maxsize=100000)
         def dfs(curSum=0):
             if curSum > target:
                 return 0 # This is not a valid solution 
             elif curSum == target:
                 return 1 # valid
-            if curSum in dp:
-                return dp[curSum]
             
             tmpSum = 0
             
@@ -21,8 +16,6 @@ class Solution:
             for n in nums:
                 if curSum + n <= target:
                     tmpSum += dfs(curSum + n)
-            
-            dp[curSum] = tmpSum
             
             return tmpSum
         
