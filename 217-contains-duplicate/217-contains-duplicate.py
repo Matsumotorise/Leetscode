32d624ec74a068c9347ab2d53bd8787da047f7b1
Andrew Chan <andrewclchan211@vt.edu>
2022-08-05 15:02:09 -0400

    Time: 674 ms (52.13%), Space: 25.8 MB (97.47%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -3,3 +3,8 @@ class Solution:
         from collections import Counter
         return any(map(lambda x: x>=2, Counter(nums).values()))
         
+        #TLE
+        #from itertools import combinations
+        #return any(x[0] == x[1] for x in combinations(nums, 2))
+        
+        
