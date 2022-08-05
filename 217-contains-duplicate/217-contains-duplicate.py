cc0cc39b94c61becd6a2a50406f325324a6cd92e
Andrew Chan <andrewclchan211@vt.edu>
2022-08-05 15:17:22 -0400

    Time: 630 ms (60.84%), Space: 26.1 MB (30.90%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -12,11 +12,22 @@ class Solution:
         return False
         '''
         
+        ''' TLE O(nlogn) time, O(1) space, inplace
         nums.sort()
         for n1, n2 in zip(nums[1:], nums):
             if n1 == n2:
                 return True
+        '''
+        nums.sort()
+        i, j = 0, 1
+        while j < len(nums):
+            if nums[i] == nums[j]:
+                return True
+            i+=1
+            j+=1
+        return False
         
+       
     
         #TLE
         #from itertools import combinations
