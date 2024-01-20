475f247ce66ee6dd4c6c8a20ad1db594d11af507
Andrew Chan <andrewclchan211@vt.edu>
2024-01-20 16:12:42 -0500

    Time: 92 ms (69.75%) | Memory: 21.2 MB (78.82%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,8 +2,8 @@ import collections
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         cntr = collections.Counter(nums)
-        sortedItems = [x[0] for x in sorted(cntr.items(), key=lambda x: x[1])]
-        return sortedItems[-k:]
+        sortedItems = [x[0] for x in sorted(cntr.items(), key=lambda x: x[1], reverse=True)]
+        return sortedItems[0:k]
 
 
         
