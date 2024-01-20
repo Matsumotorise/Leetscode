1671ed4d7fad2788f89a74ef8ba5beefb4caedfc
Andrew Chan <andrewclchan211@vt.edu>
2024-01-20 13:37:07 -0500

    Time: 27 ms (99.96%) | Memory: 16.9 MB (61.11%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,8 @@
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sCounter = collections.Counter(s)
+        tCounter = collections.Counter(t)
+        return sCounter == tCounter
+        
+        
